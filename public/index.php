8ea94762c1d36d7a67b118433c4cccc7bb6258a0
Chris Herden <c.herden@thisisdmg.com>
2013-11-07 22:58:45 +0100

    add index class and namespace loader

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Index class
+ * 
+ * @author Chris Herden <contact@chris-herden.de>
+ * @copyright (c) 2013, Chris Herden
+ */
+class Index {
+
+	/**
+	 * Perform
+	 */
+	public function __construct() {
+		$this->_checkPhpVersion();
+		$this->_loadNamespaces();
+	}
+
+	/**
+	 * Check PHP version
+	 * 
+	 * @throws Exception
+	 */
+	protected function _checkPhpVersion() {
+		if (version_compare(phpversion(), "5.3.0", "<")) {
+			throw new Exception('PHP version 5.3 or above is required to run this code. Please upgrade to continue.');
+		}
+	}
+
+	/**
+	 * Load namespaces
+	 */
+	protected function _loadNamespaces() {
+		require_once 'vendor/SplClassLoader/SplClassLoader.php';
+		$loader = new SplClassLoader('Application', '../');
+		$loader->register();
+	}
+	
+}
+
+$index = new Index();
