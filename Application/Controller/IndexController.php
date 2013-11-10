a6f37967d5f2c1d56ad63f6b84132d46294f9d2a
Chris Herden <c.herden@thisisdmg.com>
2013-11-10 01:58:23 +0100

    add index controller

---
+++ Application/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Application\Controller;
+
+/**
+ * Index controller
+ * 
+ * @author Chris Herden <contact@chris-herden.de>
+ * @copyright (c) 2013, Chris Herden
+ * @license http://opensource.org/licenses/MIT
+ */
+class IndexController {
+	
+	/**
+	 * Index Action
+	 */
+	public function indexAction() {
+		
+	}
+	
+}
