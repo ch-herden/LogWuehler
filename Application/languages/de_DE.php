c43f0726631914791d54dc38d78426ba5f2ad37b
Chris Herden <contact@chris-herden.de>
2013-11-27 21:36:19 +0100

    modify files for translation

--- Application/languages/de_DE.php
+++ Application/languages/de_DE.php
@@ -8,6 +8,8 @@
  * @license http://opensource.org/licenses/MIT
  */
 return array(
+	'cn.log.index.apache.error' => 'Apache Error Log',
+	'cn.log.index.apache.access' => 'Apache Access Log',
 	'cn.log.show.table.head.apache.error.time' => 'Zeitpunkt',
 	'cn.log.show.table.head.apache.error.level' => 'Level',
 	'cn.log.show.table.head.apache.error.message' => 'Nachricht',
@@ -16,5 +18,29 @@ return array(
 	'cn.log.show.table.head.apache.access.request' => 'Request',
 	'cn.log.show.table.head.apache.access.code' => 'HTTP Code',
 	'cn.log.show.table.head.apache.access.referer' => 'Referer',
-	'cn.log.show.table.head.apache.access.useragent' => 'Useragent'
+	'cn.log.show.table.head.apache.access.useragent' => 'Useragent',
+	'cn.log.show.filter.date.from' => 'Von:',
+	'cn.log.show.filter.date.to' => 'Bis:',
+	'cn.log.show.filter.search' => 'Suchbegriff',
+	'cn.log.show.filter.btn' => 'Anzeigen',
+	'cn.log.show.completed.noresult' => 'Keine Ergebnisse gefunden',
+	'cn.install.index.title' => 'Installation',
+	'cn.install.index.warning.https' => 'LogWuehler wurde nicht über HTTPS aufgerufen.',
+	'cn.install.index.warning.httpauth' => 'Diese Seite ist nicht über HTTP Authentifizierung geschützt.',
+	'cn.install.index.requirements.title' => 'Abhängigkeiten',
+	'cn.install.index.requirements.writeable' => 'Schreibrechte im Application Ordner',
+	'cn.install.index.requirements.phpversion' => 'PHP Version >= 5.4',
+	'cn.install.index.message.success' => 'LogWuehler wurde erfolgreich installiert.',
+	'cn.install.index.link.con' => 'Weiter',
+	'cn.install.index.message.error.title' => 'Fehler!',
+	'cn.install.index.message.error.body' => 'Die Config Datei konnte nicht angelegt werden.',
+	'cn.install.index.file.message.error.title' => 'Fehler!',
+	'cn.install.index.file.message.error.body' => 'Das Verzeichnis existiert nicht, oder es bestehen nicht die nötigen Rechte.',
+	'cn.install.index.file.apache.error.title' => 'Apache Error Log',
+	'cn.install.index.file.apache.error.dir' => 'Pfad',
+	'cn.install.index.file.apache.error.keyword' => 'Schlüsselwort',
+	'cn.install.index.file.apache.access.title' => 'Apache Access Log',
+	'cn.install.index.file.apache.access.dir' => 'Pfad',
+	'cn.install.index.file.apache.access.keyword' => 'Schlüsselwort',
+	'cn.install.index.btn.install' => 'Installieren'
 );
