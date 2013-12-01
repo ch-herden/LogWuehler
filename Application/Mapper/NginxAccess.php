5b4a81853adaba294cce20cd70c715f957ef5393
Chris Herden <contact@chris-herden.de>
2013-12-01 19:24:12 +0100

    add nginx mapper

---
+++ Application/Mapper/NginxAccess.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Mapper;
+
+/**
+ * Nginx access log file mapper
+ * 
+ * @author Chris Herden <contact@chris-herden.de>
+ * @copyright (c) 2013, Chris Herden
+ * @license http://opensource.org/licenses/MIT
+ */
+class NginxAccess extends LogFile {
+	
+	public function getProperties() {
+		
+	}
+	
+	protected function _getKeyword() {
+		
+	}
+	
+	protected function _getEntry($line, $timeStart, $timeEnd, $term) {
+		
+	}
+	
+}
---
+++ Application/Mapper/NginxError.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Mapper;
+
+/**
+ * Nginx error log file mapper
+ * 
+ * @author Chris Herden <contact@chris-herden.de>
+ * @copyright (c) 2013, Chris Herden
+ * @license http://opensource.org/licenses/MIT
+ */
+class NginxError extends LogFile {
+	
+	public function getProperties() {
+		
+	}
+	
+	protected function _getKeyword() {
+		
+	}
+	
+	protected function _getEntry($line, $timeStart, $timeEnd, $term) {
+		
+	}
+
+}
